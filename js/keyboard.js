6b867c88167363e8207da2c20d70fedc5940462b
progr <progr000@gmail.com>
2022-04-07 00:31:38 +0300

    developing visual keyboard

---
+++ js/keyboard.js
@@ -0,0 +1,55 @@
+let $keyboard_id = $('#keyboard-id');
+let current_lang = 'ru';
+
+/**
+ *
+ * @param {string} lang
+ */
+function selectCurrentKeyboard(lang)
+{
+    $keyboard_id.find('.keyboard-button').each(function () {
+        let $self = $(this);
+        $self.children('div').children('span').children('span').html($self.data(`value-${lang}`));
+    });
+}
+
+/**
+ * When the document is loaded we can start
+ */
+$(document).ready(function () {
+
+    /**/
+    $('.sel-lang').on('click', function () {
+        selectCurrentKeyboard($(this).data('lang'));
+    });
+
+    /**/
+    document.querySelector('body').addEventListener('keyup', function (e) {
+
+        /* find out which button is pressed */
+        let curPressKey = e.key.trim();
+        let curKeyCode  = e.keyCode;
+
+        if (curKeyCode === 16) {
+            $('.keyboard-keyset-default').show();
+            $('.keyboard-keyset-shift').hide();
+        }
+    });
+
+    /**/
+    document.querySelector('body').addEventListener('keydown', function (e) {
+
+        /* find out which button is pressed */
+        let curPressKey = e.key.trim();
+        let curKeyCode  = e.keyCode;
+
+        console.log(curKeyCode, curPressKey);
+
+        if (curKeyCode === 16) {
+            $('.keyboard-keyset-default').hide();
+            $('.keyboard-keyset-shift').show();
+        }
+
+    });
+
+});
